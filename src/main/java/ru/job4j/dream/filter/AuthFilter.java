4a916b9b0ddf15bd3f8bc1f2e7497bf833ff7260
ZubovVP <Zubov.VP@yandex.ru>
2021-04-07 15:49:01 +0300

    Add filter.

---
+++ src/main/java/ru/job4j/dream/filter/AuthFilter.java
@@ -0,0 +1,43 @@
+package ru.job4j.dream.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+/**
+ * Created by Intellij IDEA.
+ * User: Vitaly Zubov.
+ * Email: Zubov.VP@yandex.ru.
+ * Version: $Id$.
+ * Date: 06.04.2021.
+ */
+public class AuthFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest sreq, ServletResponse sresp, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) sreq;
+        HttpServletResponse resp = (HttpServletResponse) sresp;
+        String uri = req.getRequestURI();
+        if (uri.endsWith("auth.do")) {
+            chain.doFilter(sreq, sresp);
+            return;
+        }
+        if (req.getSession().getAttribute("user") == null) {
+            resp.sendRedirect(req.getContextPath() + "/login.jsp");
+            return;
+        }
+        chain.doFilter(sreq, sresp);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
