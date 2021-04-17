b5a8eb7723a603cf03cfa80eb02fea2aaaac7b61
ZubovVP <Zubov.VP@yandex.ru>
2021-04-17 19:59:38 +0300

    Add Ajax Jquery.

---
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -0,0 +1,29 @@
+package ru.job4j.dream.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+/**
+ * Created by Intellij IDEA.
+ * User: Vitaly Zubov.
+ * Email: Zubov.VP@yandex.ru.
+ * Version: $Id$.
+ * Date: 15.04.2021.
+ */
+public class GreetingServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("UTF-8");
+        resp.addHeader("Access-Control-Allow-Origin", "*"); // Cros
+        String email = req.getParameter("email");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.println("Your email is  " + email);
+        writer.flush();
+    }
+}
